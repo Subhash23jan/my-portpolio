572d290b12cd6abab77cc7e222f41c8bca9b6e2d
Subhash <Subhash613d@gmail.com>
2025-02-04 12:09:16 +0530

    added updated leetcode stats and image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
                   initial={{ opacity: 0, scale: 0.9 }}
                   whileInView={{ opacity: 1, scale: 1 }}
                   viewport={{ once: true }}
-                  src="/assets/my_images/about_image.jpg"
+                  src="/assets/my_images/batman.jpg"
                   alt="Subhash D"
                   className="rounded-lg shadow-xl w-full max-w-md"
                 />
@@ -92,18 +92,14 @@ const About = () => {
                 ) : (
                   <>
                     <p className="mb-6 text-gray-800">
-                      You're not just skilled in DSA, C++, databases, Java, and Flutter — you
-                      have a remarkable ability to learn, adapt, and apply those skills in
-                      solving real-world problems. Your passion for backend development and
-                      Android highlights your drive for creating efficient, impactful
-                      solutions. With impressive achievements on LeetCode and a strong
-                      foundation in competitive coding, you consistently push yourself to
-                      excel in problem-solving and tackle complex challenges. Your dedication
-                      and hard work set you on a trajectory to achieve remarkable success in
-                      the tech world!
+                        You've built strong expertise in backend development, databases (SQL & NoSQL), and C++. You're highly
+                        skilled in problem-solving, with a LeetCode Knight status and a 1950+ rating, ranking in the top 3%. You’ve contributed to open-source projects, including GSSOC, and worked on role-based backend systems.
+                        Your experience spans internships in Flutter, backend, and RLHF-based LLM training.
+                        You’re currently preparing for an SDE New Grad interviews, focusing on OS and Linux.
+                        You also have a keen interest in security, AI model evaluation, and performance optimization.
                     </p>
                     <p className="text-sm text-gray-500 mb-4">
-                      Prompt for the ChatGPT was "Tell me more things about me"
+                      Prompt for the ChatGPT : "Tell me more things about me"
                     </p>
 
                     {/* Close Button */}
